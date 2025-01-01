2f96c72d1315fe12d8286e877e30f79423d1eb77
iykghbj <149372463+iykghbj@users.noreply.github.com>
2025-01-01 18:20:33 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -46,4 +46,3 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || "Manu-MD&mUNigCiC#oK81y0sVrUjitElgJvS8gGVOAIBgH3NvYeWL7PrMj5Y", // ඔයාගෙ සෙසන් එකk දාගක්න්න / Put Your Session_id Here !!! 💚
 MONGODB: process.env.MONGODB || "mongodb+srv://manulwijethilaka42:manumd123@cluster0.le0te.mongodb.net/", // ඔයාගෙ Mongodb Url එක දාගන්න / Put Your Mongodb Url Here !!! 💚
 };
-
